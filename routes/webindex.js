a62eb55876f9d307ba0e605df84cb181db526775
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-03 20:29:40 -0600

    Set up contact form

--- routes/webindex.js
+++ routes/webindex.js
@@ -1,10 +1,22 @@
 var router = require('express').Router();
+var auth = require('../lib/auth');
 
 module.exports = () => {
 
     router.get('/', (req, res) => {
 		res.render('index/index');
     });
+
+	router.get('/contact', (req, res) => {
+		auth.generateToken()
+			.then((token) => res.render('index/contact', { token }))
+			.catch((err) => res.status(500).send(err));
+	});
+
+	router.post('/contact', (req, res) => {
+		console.log('CONTACT FORM POSTED', req.body);
+		res.sendStatus(200);
+	});
 
     return router;
 };
