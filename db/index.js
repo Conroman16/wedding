73d17d2bcbb2d7eae295cedf3ec6cd25f9cf6840
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-03 18:15:44 -0600

    Cleanup db index file

--- db/index.js
+++ db/index.js
@@ -1,23 +1,23 @@
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var allConfig = require('./config.json');
-var db        = {};
 var appConfig = require('../lib/config');
+var dbConfig = require('./config.json');
+var config = {};
+var db = {};
 
 if (!process.env.DATABASE_URL)
-	var config = allConfig[process.env.NODE_ENV];
+	config = dbConfig[process.env.NODE_ENV];
 else {
-	var dbUrlMatch = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-	var config = {
-		dialect: 'postgres',
-		protocol: 'postgres',
-		database: dbUrlMatch[5],
-		username: dbUrlMatch[1],
-		password: dbUrlMatch[2],
-		host: dbUrlMatch[3],
-		port: dbUrlMatch[4],
+	let dbUrlMatch = process.env.DATABASE_URL.match(/(\w+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+	config = {
+		dialect: dbUrlMatch[1],
+		protocol: dbUrlMatch[1],
+		database: dbUrlMatch[6],
+		username: dbUrlMatch[2],
+		password: dbUrlMatch[3],
+		host: dbUrlMatch[4],
+		port: dbUrlMatch[5],
 		logging: appConfig.isDev,
 		dialectOptions: {
 			ssl: true
@@ -25,24 +25,18 @@ else {
 	};
 }
 
-if (config.use_env_variable)
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-else
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-fs
-  .readdirSync(path.join(__dirname, 'models'))
-  .filter((file) => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach((file) => {
-    var model = sequelize['import'](path.join(__dirname, 'models', file));
-    db[model.name] = model;
-  });
+fs.readdirSync(path.join(__dirname, 'models'))
+	.filter((file) => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
+	.forEach((file) => {
+		let model = sequelize['import'](path.join(__dirname, 'models', file));
+		db[model.name] = model;
+	});
 
 Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate)
-    db[modelName].associate(db);
+	if (db[modelName].associate)
+		db[modelName].associate(db);
 });
 
 db.sequelize = sequelize;
