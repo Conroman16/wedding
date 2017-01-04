ff258724e274ff05c788f29c37465994154849ea
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-03 19:06:51 -0600

    Add about page for gathering information about Heroku dyno

--- routes/about.js
+++ routes/about.js
@@ -1,10 +1,16 @@
 var os = require('os');
 var router = require('express').Router();
+var _ = require('underscore');
 
 module.exports = () => {
 
-	router.get('/cpus', (req, res) => {
-		res.send(os.cpus());
+	router.get('/', (req, res) => {
+		var cpus = os.cpus();
+		var items = [
+			{ key: 'cpu', value: cpus[0].model },
+			{ key: 'cores', value: cpus.length }
+		];
+		res.render('about/index', { items: items });
 	});
 
 	return router;
