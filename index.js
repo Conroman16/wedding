8399e364ad47b7844a209557469755ec039a8985
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-05 13:59:53 -0600

    Set up app as PM2 cluster

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 var pm2 = require('pm2');
 var os = require('os');
+var events = require('./lib/events');
 
-var memory = process.env.WEB_MEMORY || 512;
+var memory = process.env.WEB_MEMORY || 512; // Megabytes
 var instances = os.cpus().length > 4 ? 4 : os.cpus().length;
+var usePm2Daemon = true;
 
-pm2.connect(() => {
+pm2.connect(!usePm2Daemon, () => {
 	pm2.start({
 		script: 'app.js',
 		name: 'dkmerger',
 		exec_mode: 'cluster',
 		instances: instances,
-		max_memory_restart: memory
+		max_memory_restart: `${memory}M`,
+		env: process.env
 	}, (err) => {
 		if (err)
 			return console.error(err.stack || err);
-		console.log('PM2 and app have started successfully');
 
+		events.registerExitEvents(process);
 		pm2.launchBus((err, bus) => {
 			if (err)
 				return console.error(err.stack || err);
-			bus.on('log:out', (packet) => console.log('[App:%s] %s', packet.process.name, packet.data));
-			bus.on('log:err', (packet) => console.error('[App:%s][Err] %s', packet.process.name, packet.data));
+			bus.on('log:out', (packet) => console.log('[%s]:[log] %s', packet.process.name, packet.data));
+			bus.on('log:err', (packet) => console.error('[%s]:[err] %s', packet.process.name, packet.data));
 		});
 	});
 });
