9bc3f2c89eaef834a5b582f4c5a54662c3f2ee57
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-06 14:42:28 -0600

    Add more info to server page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let pm2 = require('pm2');
 let os = require('os');
 let events = require('./lib/events');
+let config = require('./lib/config');
 
 let memory = process.env.WEB_MEMORY || 512; // Megabytes
 let instances = os.cpus().length > 4 ? 4 : os.cpus().length;
@@ -9,7 +10,7 @@ let usePm2Daemon = true;
 pm2.connect(!usePm2Daemon, () => {
 	pm2.start({
 		script: 'app.js',
-		name: 'dkmerger',
+		name: config.appName,
 		exec_mode: 'cluster',
 		instances: instances,
 		max_memory_restart: `${memory}M`,
