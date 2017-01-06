978f16fb25b27e9d95325d5cebbf440de15d233d
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-06 14:42:28 -0600

    Add more info to server page

--- routes/server.js
+++ routes/server.js
@@ -1,26 +1,27 @@
 let os = require('os');
 let router = require('express').Router();
 let pm2 = require('pm2');
+let config = require('../lib/config');
 
 module.exports = () => {
 
 	router.get('/', (req, res) => {
-		pm2.list((err, procs) => {
+		pm2.describe(config.appName, (err, description) => {
 			if (err)
 				return res.status(500).send(err);
 
 			let cpus = os.cpus();
+			let pm_env = description[0].pm2_env;
 			let items = [
 				{ key: 'cpu', value: cpus[0].model },
 				{ key: 'cores', value: cpus.length },
-				{ key: 'cluster nodes', value: procs.length }
+				{ key: 'cluster nodes', value: pm_env.instances },
+				{ key: 'cwd', value: pm_env.pm_cwd },
+				{ key: 'exec', value: pm_env.pm_exec_path },
+				{ key: 'unstable restarts', value: pm_env.unstable_restarts }
 			];
 			res.render('server/about', { items: items });
 		});
-	});
-
-	router.get('/pm2', (req, res) => {
-
 	});
 
 	return router;
