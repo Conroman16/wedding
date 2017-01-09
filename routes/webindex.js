d629ee0ab25fbef302fbe1a26f1bdc879aba0688
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-08 22:24:09 -0600

    Use redis for form auth tokens

--- routes/webindex.js
+++ routes/webindex.js
@@ -1,22 +1,10 @@
 let router = require('express').Router();
-let auth = require('../lib/auth');
 
 module.exports = () => {
 
     router.get('/', (req, res) => {
 		res.render('index/index');
     });
-
-	router.get('/rsvp', (req, res) => {
-		auth.generateToken()
-			.then((token) => res.render('index/rsvp', { token }))
-			.catch((err) => res.status(500).send(err));
-	});
-
-	router.post('/rsvp', (req, res) => {
-		console.log('RSVP POSTED', req.body);
-		res.sendStatus(200);
-	});
 
     return router;
 };
