765b6ffe0acc3711e7db92df65d3b4d61a9ffcec
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-11 22:16:20 -0600

    Delete old deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# Change permissions on logs
-chmod 777 /tmp/pm2* -R
-
-# Install npm packages
-echo "Installing npm packages"
-npm install
-
-# Start the app
-pm2 startOrRestart ecosystem.json --env production
