8ed362971cc229e5fe2d4662db2fd45f83db6afc
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-19 17:26:17 -0600

    Lazyload images

---
+++ static/js/lazyload.js
@@ -0,0 +1,28 @@
+(function(){
+	var head = document.head || document.getElementsByTagName('head')[0];
+
+	window.lazyloader = {
+		init: function(){
+			var llImgs = $('.bg-lazyload');
+			$.each(llImgs, function(indx){
+				var $img = $(llImgs[indx]),
+					fsUrl = $img.data('url'),
+					styleEl = document.createElement('style'),
+					img = new Image(),
+					cssText = window.templates.llImgTransition({ imgUrl: fsUrl });
+
+				if (styleEl.styleSheet)
+					styleEl.styleSheet.cssText = cssText;
+				else
+					styleEl.appendChild(document.createTextNode(cssText));
+				head.appendChild(styleEl);
+
+				img.onload = function(){
+					$img.css('background-image', 'url(' + fsUrl + ')');
+					$img.addClass('ll-loaded');
+				};
+				img.src = fsUrl;
+			});
+		}
+	};
+})();
