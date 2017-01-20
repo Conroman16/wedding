a6d34d92ee3b41b17e893fb8493681daa354e38e
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-20 17:20:25 -0600

    Revert "Remove dependency on sqlite3 since we're not using it anymore"
    This reverts commit f474b26e16eb04d9841a11583225480ff5c7e50d.

--- db/index.js
+++ db/index.js
@@ -2,22 +2,28 @@ let fs = require('fs');
 let path = require('path');
 let Sequelize = require('sequelize');
 let appConfig = require('../lib/config');
+let dbConfig = require('./config.json');
+let config = {};
 let db = {};
 
-let dbUrlMatch = process.env.DATABASE_URL.match(/(\w+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
-let config = {
-	dialect: dbUrlMatch[1],
-	protocol: dbUrlMatch[1],
-	database: dbUrlMatch[6],
-	username: dbUrlMatch[2],
-	password: dbUrlMatch[3],
-	host: dbUrlMatch[4],
-	port: dbUrlMatch[5],
-	logging: appConfig.isDev ? false : undefined,
-	dialectOptions: {
-		ssl: true
-	}
-};
+if (!process.env.DATABASE_URL)
+	config = dbConfig[process.env.NODE_ENV];
+else {
+	let dbUrlMatch = process.env.DATABASE_URL.match(/(\w+):\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+	config = {
+		dialect: dbUrlMatch[1],
+		protocol: dbUrlMatch[1],
+		database: dbUrlMatch[6],
+		username: dbUrlMatch[2],
+		password: dbUrlMatch[3],
+		host: dbUrlMatch[4],
+		port: dbUrlMatch[5],
+		logging: appConfig.isDev ? false : undefined,
+		dialectOptions: {
+			ssl: true
+		}
+	};
+}
 
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
