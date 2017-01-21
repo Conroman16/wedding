74b67729228c29df5c9b551e95110ad3f0079708
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-21 16:18:05 -0600

    Remove dependency on sqlite3 but keep ability to use sequelize config.js

---
+++ db/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	// Install 'sqlite3' if you want to uncomment these
+	// "development": {
+	// 	"dialect": "sqlite",
+	// 	"storage": "./data.dev.db"
+	// },
+	// "production": {
+	// 	"dialect": "sqlite",
+	// 	"storage": "./data.db",
+	// 	"logging": false
+	// }
+};
