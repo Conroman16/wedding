0ee2f096cf051e961c67ef94ca04acf295e862f0
Connor Kennedy <cjtkennedy@gmail.com>
2017-01-21 23:36:03 -0600

    Add twilio for SMS connectivity

--- routes/webindex.js
+++ routes/webindex.js
@@ -1,10 +1,17 @@
 let router = require('express').Router();
+let sms = require('../lib/sms');
 
 module.exports = () => {
 
     router.get('/', (req, res) => {
 		res.render('index/index');
     });
+
+    router.get('/smstest', (req, res) => {
+		sms.send('***REMOVED***', 'Testing 1..2...3....')
+			.then((response) => res.send(response))
+			.catch((error) => res.status(500).send(error));
+	});
 
     return router;
 };
