cc36469e2ec35c6947d123197c3d530a88c539bd
Connor Kennedy <cjtkennedy@gmail.com>
2017-02-24 12:49:17 -0600

    Checking out opbeat

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ if (!process.env.NODE_ENV){
 
 console.log(`${process.env.NODE_ENV.toUpperCase()} MODE`);
 
+let config = require('./lib/config');
+let opbeat = require('opbeat');
+// if (config.isProd){
+	 opbeat.start({
+		appId: '19adbb6fb5',
+		organizationId: 'dd519ce34581415b8fb59720a762194d',
+		secretToken: '46c0cbaf5b5c63cff6ad0115f37a67326cb35e42'
+	});
+// }
+
 require('./lib/extensions');  // Configure extensions
 let db = require('./db'),
 	server = require('./lib/server'),
