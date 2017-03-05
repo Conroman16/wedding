8bf9f041dba4ea8e571b4b24c0a173a7ff945d43
Connor Kennedy <cjtkennedy@gmail.com>
2017-03-05 14:37:43 -0600

    Fix up tabs switcher logic

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -1,54 +1,38 @@
-$(document).ready(function(){
+$(function(){
+	var slug = window.location.hash.replace('#', '');
+	var $el = $('.active-default');
+	var tabWrapper = $('.tab__content');
 
-	// Variables
-	var clickedTab = $(".tabs > .active");
-	var tabWrapper = $(".tab__content");
-	var activeTab = tabWrapper.find(".active");
+	if (slug){
+		$el = $('.tab-item[data-slug="' + slug + '"]');
+		$el.addClass('active');
+	}
+	else
+		$el.toggleClass('active-default active');
+
+	var activeTab = tabWrapper.find('.active');
 	var activeTabHeight = activeTab.outerHeight();
 
-	// Show tab on page load
 	activeTab.show();
-
-	// Set height of wrapper on page load
 	tabWrapper.height(activeTabHeight);
 
-	$(".tabs > .tab-item").on("click", function() {
+	$('.tabs > .tab-item').on('click', function() {
+		$('.tabs .tab-item').removeClass('active');
+		var $this = $(this);
+		$this.addClass('active');
+		window.location.hash = $this.data('slug');
 
-		// Remove class from active tab
-		$(".tabs > .tab-item").removeClass("active");
-
-		// Add class active to clicked tab
-		$(this).addClass("active");
-
-		// Update clickedTab variable
-		clickedTab = $(".tabs .active");
-
-		// fade out active tab
 		activeTab.fadeOut(250, function() {
-
-			// Remove active class all tabs
-			$(".tab__content > .tab-item").removeClass("active");
-
-			// Get index of clicked tab
-			var clickedTabIndex = clickedTab.index();
-
-			// Add class active to corresponding tab
-			$(".tab__content > .tab-item").eq(clickedTabIndex).addClass("active");
-
-			// update new active tab
-			activeTab = $(".tab__content > .active");
-
-			// Update variable
+			$('.tab__content .tab-item').removeClass('active');
+			activeTab = $('.tab__content .tab-item[data-slug="' + $this.data('slug') + '"]');
+			activeTab.addClass('active');
 			activeTabHeight = activeTab.outerHeight();
 
 			// Animate height of wrapper to new tab height
 			tabWrapper.stop().delay(50).animate({
 				height: activeTabHeight
 			}, 500, function() {
-
-				// Fade in active tab
 				activeTab.delay(50).fadeIn(250);
-
 			});
 		});
 	});
